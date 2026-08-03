dfb8ffdc30ac9ac5e77d7bf24d82975b1a973dcc
Missy Restless <missyrestless@gmail.com>
2026-08-03 08:54:33 -0700

    Add examples to view_chat usage, check for avatar chat folder

--- view_chat.sh
+++ view_chat.sh
@@ -10,7 +10,7 @@
 # Set to the default avatar folder you wish to use, dot replaced by underscore in Avatar name
 DEF_AVI="missy_restless"
 # Set one or both of these, Mac and/or Linux, to the default chat log you wish to view
-MAC_DEF_CHAT="SierraAngelic"
+MAC_DEF_CHAT="Katia108"
 LIN_DEF_CHAT="Doc Rae"
 ## END User defined variables
 
@@ -20,7 +20,7 @@ MAC_DIR="/Users/${USER}/Library/Application Support/Firestorm"
 LIN_DIR="/home/${USER}/.firestorm_x64"
 
 usage() {
-  printf "\nUsage: view_chat [-a my_avatar] [-c their_avatar] [-eflt] [-s search] [-h]"
+  printf "Usage: view_chat [-a my_avatar] [-c their_avatar] [-eflt] [-s search] [-h]"
   printf "\n\tDisplay chat or teleport history"
   printf "\nWhere:"
   printf "\n\t-a my_avatar specifies your avatar name [default: ${DEF_AVI}]"
@@ -31,9 +31,24 @@ usage() {
   printf "\n\t-l indicates list files containing search term"
   printf "\n\t-s search specifies a search term"
   printf "\n\t-t indicates display teleport history"
-  printf "\n\t-h displays this usage message and exits\n\n"
+  printf "\n\t-h displays this usage message and exits"
+  printf "\n\tChat Folder Path: ${FDIR}/${AVI}"
+  printf "\n\tAvatar Chat Log: ${CHAT}"
+  printf "\nExamples:"
+  printf "\n\tDisplay default chat log with avatar Easy Islay:"
+  printf "\n\t\tview_chat -c \"Easy Islay\""
+  printf "\n\tDisplay chat log for avatar anya.ordinary with avatar Missy Restless:"
+  printf "\n\t\tview_chat -a anya_ordinary -c \"Missy Restless\""
+  printf "\n\tList chat logs matching search term \"doc\":"
+  printf "\n\t\tview_chat -f -s doc"
+  printf "\n\tDisplay chat with avatar \"Easy Islay\" matching search term \"send\":"
+  printf "\n\t\tview_chat -c \"Easy Islay\" -s send\n"
   exit 1
 }
+
+info() { printf "%b[Info]%b %s\n" '\e[0;32m\033[1m' '\e[0m' "$*" >&2; }
+warn() { printf "%b[Warn]%b %s\n" '\e[0;33m\033[1m' '\e[0m' "$*" >&2; }
+erro() { printf "%b[Error]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$*" >&2; }
 
 AVI= AVI_CHAT= CHAT= EDIT_MODE= FIND_CHAT= TP_HIST= GREP_OPTS= SEARCH= SHOW_USAGE=
 while getopts ":a:c:efils:thu" flag; do
@@ -67,14 +82,12 @@ while getopts ":a:c:efils:thu" flag; do
       SHOW_USAGE=1
       ;;
     \?)
-      echo "Invalid option: $flag"
+      erro "Invalid option: $flag"
       SHOW_USAGE=1
       ;;
   esac
 done
 shift $(( OPTIND - 1 ))
-
-[ "${AVI}" ] || AVI="${DEF_AVI}"
 
 platform=$(uname -o)
 if [ "${platform}" == "Darwin" ]; then
@@ -87,10 +100,41 @@ else
   [ "${CHAT}" ] || CHAT="${LIN_DEF_CHAT}"
 fi
 
+[ "${AVI}" ] || {
+  # Use the default avatar name or, if not found, the latest folder we find
+  AVI="${DEF_AVI}"
+  [ -d "${FDIR}/${AVI}" ] || {
+    AVI=$(echo "${AVI}" | sed -e "s/\./_/g")
+    [ -d "${FDIR}/${AVI}" ] || {
+      warn "Cannot locate default Avatar chat folder ${FDIR}/${AVI}"
+      avi_dir=
+      while read adir
+      do
+      [ "${adir}" == "browser_profile" ] && continue
+      [ "${adir}" == "cache" ] && continue
+      [ "${adir}" == "data" ] && continue
+      [ "${adir}" == "logs" ] && continue
+      [ "${adir}" == "user_settings" ] && continue
+      [ -f "${FDIR}/${adir}/screen_last.png" ] || [ -f "${FDIR}/${adir}/screen_home.png" ] && {
+        avi_dir="${adir}"
+        break
+      }
+      done < <(/bin/ls -1t -- "${FDIR}")
+      if [ "${avi_dir}" ]; then
+        AVI="${avi_dir}"
+        info "Using latest existing chat folder ${FDIR}/${AVI}"
+      else
+        warn "Cannot locate any chat folders in ${FDIR}"
+      fi
+    }
+  }
+}
+echo ""
+
 [ -d "${FDIR}/${AVI}" ] || {
   AVI=$(echo "${AVI}" | sed -e "s/\./_/g")
   [ -d "${FDIR}/${AVI}" ] || {
-    echo "ERROR: cannot locate Avatar log folder ${FDIR}/${AVI}"
+    erro "Cannot locate Avatar chat folder ${FDIR}/${AVI}"
     SHOW_USAGE=1
   }
 }
@@ -110,7 +154,7 @@ fi
 if [ "${SEARCH}" ]; then
   cd "${FDIR}/${AVI}"
   if [ "${FIND_CHAT}" ]; then
-    /bin/ls -1 *.txt | while read txt
+    /bin/ls -1 -- *.txt | while read txt
     do
       echo "${txt}" | grep -i "${SEARCH}" >/dev/null && echo "${txt}"
     done
@@ -127,7 +171,7 @@ if [ "${SEARCH}" ]; then
   fi
 else
   [ -f "${FDIR}/${AVI}/${CHAT}" ] || {
-    echo "ERROR: cannot locate Avatar chat log ${FDIR}/${AVI}/${CHAT}"
+    erro "Cannot locate Avatar chat log ${FDIR}/${AVI}/${CHAT}"
     usage
   }
   if [ "${EDIT_MODE}" ]; then
